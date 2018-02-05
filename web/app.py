f92e4a13cad74bbf709ee190b78fb7f99380501f
Zubin Mehta <zubin.mehta1996@gmail.com>
2018-02-05 18:23:14 -0500

    Added registration Added database connection

--- web/app.py
+++ web/app.py
@@ -6,6 +6,7 @@ from flask_wtf.file import FileField
 from wtforms import TextField, HiddenField, ValidationError, RadioField,\
     BooleanField, SubmitField, PasswordField, FormField, validators
 from wtforms.validators import Required
+import psycopg2
 
 app = Flask(__name__)
 Material(app)
@@ -13,11 +14,14 @@ app.config['SECRET_KEY'] = 'USE-YOUR-OWN-SECRET-KEY-DAMNIT'
 app.config['RECAPTCHA_PUBLIC_KEY'] = 'TEST'
 
 # straight from the wtforms docs:
-class LoginForm(FlaskForm):
+class LoginForm(FlaskForm): 
     username = TextField('Username', [validators.required()])
     password = PasswordField('Password', [validators.required()])
     submit = SubmitField(label="Log In")
-
+class RegisterForm(FlaskForm): 
+    username = TextField('Username', [validators.required()])
+    password = PasswordField('Password', [validators.required()])
+    submit = SubmitField(label="Create Account")
 class ExampleForm(FlaskForm):
     field1 = TextField('First Field', description='This is field one.')
     field2 = TextField('Second Field', description='This is field two.',
@@ -32,7 +36,7 @@ class ExampleForm(FlaskForm):
     checkbox_field = BooleanField('This is a checkbox',
                                   description='Checkboxes can be tricky.')
 
-    ff = FileField('Sample upload')
+    photo = FileField('Sample upload')
 
     submit_button = SubmitField('Submit Form')
 
@@ -40,20 +44,32 @@ class Image():
     def __init__(self, description):
         self.description = description
 
-@app.route('/', methods=['GET', 'POST'])  
+def get_database():
+    conn = psycopg2.connect("dbname='flask' user='flask' host='db'")
+    conn.autocommit = True
+    return conn, conn.cursor()
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     form = LoginForm()
     if request.method == 'POST':
         app.logger.info(request.values)
     return render_template('login.html', form=form)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST':
+        app.logger.info(request.values)
+        con, cursor = get_database()
+        cursor.execute("""INSERT INTO public.users VALUES(%s, %s)""", (request.values.get('username')
+        , request.values.get('password')))
+        cursor.close()
+    return render_template('register.html', form=form)
+
 @app.route('/home')
 def home(page=1):
     images = []
-    for i in range(100):
-        images.append(Image(str(i)))
-    images.append(Image("HELLO"))
-    images.append(Image("HOW ARE YOU"))
     page = request.args.get(get_page_parameter(), type=int, default=1)
     pagination = Pagination(page=page, per_page=10, total=len(images), search=False, record_name='images')
     return render_template('home.html', images=images, per_page=10, pagination=pagination)
@@ -61,11 +77,11 @@ def home(page=1):
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     if request.method == 'POST':
-        app.logger.info(request.files.get('file'))
+        app.logger.info(request.files)
     form = ExampleForm()
     return render_template('upload.html', form=form)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
   
